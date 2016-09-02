db03e0000322958b47f358b1788e29a8d5e209ff
Dave English <dave@lutnos.com>
2016-09-02 15:10:27 +0200

    Implemented service price

--- app/controllers/checkout.scala
+++ app/controllers/checkout.scala
@@ -3,13 +3,15 @@ package controllers
 import javax.inject.{Inject, Singleton}
 
 import play.api.mvc.{Action, Controller}
+import services.Pricing._
 
 @Singleton
 class Checkout @Inject() extends Controller {
 
   def price(items: String) = Action {
     val itemsList = items.toLowerCase.split(",").toList
-    Ok("")
+    Ok(totalPrice(items).formatted("%.2f"))
+
   }
 
 }
